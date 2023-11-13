ee19c0c2a93389ab948856c91d351790384d5801
gurvindersingh9803 <gurvindersingh9803@gmail.com>
2023-11-13 17:07:33 -0500

    Bottom nav

---
+++ app/src/main/java/com/example/kathavichar/common/BottomNavigationScreens.kt
@@ -0,0 +1,3 @@
+package com.example.kathavichar.common
+
+class BottomNavigationScreens{}
---
+++ app/src/main/java/com/example/kathavichar/common/NavigationController.kt
@@ -0,0 +1,25 @@
+package com.example.kathavichar.common
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+
+@Composable
+fun MainActivityScreen() {
+    Text("Main")
+}
+
+@Composable
+fun DownloadScreen() {
+    Text("Download")
+}
+
