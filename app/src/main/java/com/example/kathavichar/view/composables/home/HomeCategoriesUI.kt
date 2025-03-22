cee0545ca7304e2e3188bd6cab2c3ca02f5c7a9e
gurvindersingh9803 <gurvindersingh9803@gmail.com>
2025-03-22 01:02:19 -0400

    new home view implemented

--- app/src/main/java/com/example/kathavichar/view/composables/home/HomeCategoriesUI.kt
+++ app/src/main/java/com/example/kathavichar/view/composables/home/HomeCategoriesUI.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -28,7 +31,7 @@ import coil.compose.AsyncImage
 import com.example.kathavichar.common.Screen
 import com.example.kathavichar.model.ArtistsItem
 
-@Composable
+/*@Composable
 fun HomeCategories(data: List<ArtistsItem>?, navigationController: NavHostController) {
     Column {
         data?.forEach { sectionData ->
@@ -39,19 +42,44 @@ fun HomeCategories(data: List<ArtistsItem>?, navigationController: NavHostContro
             Spacer(modifier = Modifier.height(15.dp))
             Column {
                 LazyRow(content = {
-                    /*items(sectionData.data) {
+                    *//*items(sectionData.data) {
                         Spacer(modifier = Modifier.width(8.dp))
                         Column {
                             PlayListItem(sectionData.data[it], navigationController)
                         }
-                    }*/
+                    }*//*
 
                     items(data.size) { index ->
+                        println("sdgrfed $index")
                         PlayListItem(sectionData, navigationController, imageUrl)
                     }
                 })
             }
             Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+}*/
+
+@Composable
+fun HomeCategories(data: List<ArtistsItem>?, navigationController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+    ) {
+        data?.let { artists ->
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2), // 2 items per row
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.height(400.dp) // Adjust height as needed
+            ) {
+                items(artists.size) { index ->
+                    val sectionData = artists[index]
+                    val imageUrl = sectionData.imgurl.replace("127.0.0.1", "10.0.2.2")
+                    PlayListItem(sectionData, navigationController, imageUrl)
+                }
+            }
         }
     }
 }
